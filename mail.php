4980bc6e24965ede72ba556340a371e1a0fcf004
DOLAP-SABANCI <78279537+DOLAP-SABANCI@users.noreply.github.com>
2021-04-01 22:48:39 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_POST["send message"]) {
+if($_POST["EmailForm"]) {
 
 	mail("sinanyildirim@sabanciuniv.edu", "Here is the subject line",
 	$_POST["insert your message here"]. "From: a.anasti@gmail.com");
